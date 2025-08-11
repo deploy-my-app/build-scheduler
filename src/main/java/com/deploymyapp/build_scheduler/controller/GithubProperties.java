961de98aac61c2d1c4b2cbdf9f64df05b926db25
Shyndard <10274648+Shyndard@users.noreply.github.com>
2025-08-10 20:54:00 -0400

    feat(events): Listen github app webhook events (#2)

---
+++ src/main/java/com/deploymyapp/build_scheduler/controller/GithubProperties.java
@@ -0,0 +1,19 @@
+package com.deploymyapp.build_scheduler.controller;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configuration
+@ConfigurationProperties(prefix = "github")
+@Getter
+@Setter
+public class GithubProperties {
+
+    private String webhookSecret;
+    private String appId;
+    private String appKey;
+
+}
